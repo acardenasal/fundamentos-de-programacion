dde61083eb474f36fad773bd587c9988737cdabe
acardenasal <43186784+acardenasal@users.noreply.github.com>
2018-09-11 15:27:20 -0500

    Create problema del rectangulo cuadrado*.py

---
+++ problema del rectangulo cuadrado*.py
@@ -0,0 +1,12 @@
+x =input("Ingrese un numero real \n")
+y =input("Ingrese un numero real \n")
+z =input("Ingrese un numero real \n")
+w =input("Ingrese un numero real \n")
+if x==y==z==w:
+    print(x*x)
+    if (x*x)>100:
+        print("es mayor")
+    elif:
+        print("es menor")
+else:
+    print(No es cuadrado)
