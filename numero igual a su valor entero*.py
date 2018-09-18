ea1cb825f2e9d3d07f79a5c1e2ea6e6a5257adeb
acardenasal <43186784+acardenasal@users.noreply.github.com>
2018-09-18 07:44:52 -0500

    Update numero igual a su valor entero*.py

--- numero igual a su valor entero*.py
+++ numero igual a su valor entero*.py
@@ -1,5 +1,5 @@
-x =input("Ingrese un numero real \n")
-if int(x) == float(x):
+x =float(input("Ingrese un numero real: \n"))
+if float(x) == int(x):
     print("Es entero")
     if x%5 == 0:
         print("Es divisible")
